ddd943514eac01464c2505a9c3abbc434ca4f092
Alan Sampson <alansamps@gmail.com>
2021-02-21 12:40:05 -0800

    Initial Commit

---
+++ CF.BookReport00/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CF.BookReport00
+//
+//  Created by Alan Sampson on 2/21/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
